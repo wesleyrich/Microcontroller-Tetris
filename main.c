016396a85e3a7708c3fa404d8a6031607128a1e3
grantjulian <49376488+grantjulian@users.noreply.github.com>
2019-11-02 15:54:59 -0400

    Updated main
    Added TIM2 functionality to main instead of led_matrix

--- main.c
+++ main.c
@@ -3,23 +3,48 @@
   * @file    main.c
   * @author  Ac6
   * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
+  * @date    11/2/19
+  * @brief   Main function for Tetris Game
   ******************************************************************************
 */
 
 
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
+#include "stdint.h"
 			
+void tim2_setup();
 
-
+#define TIM2_FREQ 12000000 // 12 MHz
 
 int main(void)
 {
 	LED_pins_setup();
-	tim6_setup();
+	initialize_pixels();
+	tim2_setup();
 	for(;;);
+}
+
+void tim2_setup ()
+{
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // enable clock to tim6
+
+	// set up the clock frequency
+	TIM2->ARR = 1;
+	TIM2->PSC = (int)(24000000 / TIM2_FREQ) - 1; //assuming ARR is '1'
+
+	TIM2->DIER |= TIM_DIER_UIE; 		// enable the update interrupt
+	TIM2->CR1 |= TIM_CR1_CEN; 			// enable the timer counter
+	NVIC->ISER[0] = 1 << TIM2_IRQn;     // enable the interrupt
+}
+
+void TIM2_IRQHandler ()
+{
+	TIM2->SR &= ~TIM_SR_UIF; // acknowledge the interrupt
+
+	update_led();			 // update the led matrix
+
+	update_tetris();		 // update the game
 }
 
 
